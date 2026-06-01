b1d1e99130fdd82060ba689dcbaab5c5656b0dac
godwell88 <godwellmahoso01@gmail.com>
2026-06-01 23:15:23 +0530

    Resolve merge conflict

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/seltad/",
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 3a49674a0c4d7f7fd254d395e971c1d861634d4e
